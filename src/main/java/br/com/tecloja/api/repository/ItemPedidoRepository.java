32c1186e00f48edee5b075fedc4503b9d974abf9
arthurproencadoracio <arthurdoracio@gmail.com>
2026-06-15 20:47:00 -0300

    Oi

--- src/main/java/br/com/tecloja/api/repository/ItemPedidoRepository.java
+++ src/main/java/br/com/tecloja/api/repository/ItemPedidoRepository.java
@@ -2,7 +2,5 @@ package br.com.tecloja.api.repository;
 
 import br.com.tecloja.api.model.ItemPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {}
--- src/main/java/br/com/tecloja/api/repository/ProdutoRepository.java
+++ src/main/java/br/com/tecloja/api/repository/ProdutoRepository.java
@@ -4,10 +4,8 @@ import br.com.tecloja.api.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
     List<Produto> findByCategoriaId(Long categoriaId);
